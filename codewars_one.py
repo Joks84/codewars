c3946cac15619faf86ebe749ff76299eaab122af
jelena <jelena.jokanovic@yahoo.com>
2021-05-21 09:17:31 +0800

    Pushing changes

--- codewars_one.py
+++ codewars_one.py
@@ -39,3 +39,81 @@ def accum(word):
 accum('abcd')
 accum('RqaEzty')
 accum('cwAt')
+
+# You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to
+# an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with
+# a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings
+# representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter
+# (direction) and you know it takes you one minute to traverse one city block, so create a function that will return
+# true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, '
+# of course, return you to your starting' point. Return false otherwise.
+#
+# Note: you will always receive a valid array containing a random assortment of direction letters
+# ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+def is_valid_walk(walk):
+    if len(walk) == 10:
+        if walk.count('n') == walk.count('s') and walk.count('w') == walk.count('e'):
+            print(True)
+        else:
+            print(False)
+    print(False)
+
+is_valid_walk(['n', 's', 'e'])
+is_valid_walk(['s', 's', 'e', 'e', 'w', 's'])
+
+
+# Given: an array containing hashes of names
+# Return: a string formatted as a list of names separated by commas except for the last two names,
+# which should be separated by an ampersand.
+# EXAMPLE:
+# namelist([ {'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'} ])
+# returns 'Bart, Lisa & Maggie'
+
+# namelist([ {'name': 'Bart'}, {'name': 'Lisa'} ])
+# returns 'Bart & Lisa'
+
+# namelist([ {'name': 'Bart'} ])
+# returns 'Bart'
+
+# namelist([])
+# returns ''
+
+def namelist(names):
+    words = ""
+    if not names:
+        print(words)
+    else:
+        raw = ""
+        for item in names:
+            for value in item.values():
+                if len(names) == 1:
+                    words += value
+                else:
+                    raw += value + " & "
+                    words = raw.replace(' & ', ', ', len(names) - 2).strip(' & ')
+    print(words)
+
+
+namelist([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}, {'name': 'Homer'}, {'name': 'Marge'}])
+namelist([{'name': 'Bart'}])
+namelist([])
+
+# Your goal in this kata is to implement a difference function,
+# which subtracts one list from another and returns the result.
+
+# It should remove all values from list a, which are present in list b keeping their order.
+# array_diff([1,2],[1]) == [2]
+
+# If a value is present in b, all of its occurrences must be removed from the other:
+# array_diff([1,2,2,2,3],[2]) == [1,3]
+
+def array_diff(a, b):
+    out = []
+    for item in a:
+        if not item in b:
+            out.append(item)
+    print(out)
+
+array_diff([1, 2], [1])
+array_diff([1, 2, 2, 2, 3], [2])
+array_diff([1, 2, 2], [1])
